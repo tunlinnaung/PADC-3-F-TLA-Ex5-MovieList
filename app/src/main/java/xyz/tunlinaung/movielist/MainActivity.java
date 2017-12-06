4d9b309e45be8d995b9dbff146c1e2948b89117f
tunlinaung <tunlinaung.java@gmail.com>
2017-12-06 23:22:05 +0630

    implement design part

--- app/src/main/java/xyz/tunlinaung/movielist/MainActivity.java
+++ app/src/main/java/xyz/tunlinaung/movielist/MainActivity.java
@@ -4,39 +4,53 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.rv_movie_list) RecyclerView rvMovieList;
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+
+    @BindView(R.id.fab) FloatingActionButton fab;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ButterKnife.bind(this, this);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @OnClick(R.id.fab)
+    public void onClickFab(View view)
+    {
+        Snackbar.make(view, "Search function is in progress. Coming soon.", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
